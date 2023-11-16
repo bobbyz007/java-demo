c72f92d66922c50dbb2485c3f086bd8a909fb556
justin <pulchritude007@163.com>
2023-11-16 19:43:30 +0800

    fix

--- gradle-tutorials/shared/build.gradle.kts
+++ gradle-tutorials/shared/build.gradle.kts
@@ -1,5 +1,25 @@
+import org.apache.commons.codec.binary.Base64
+
+buildscript {
+    repositories {
+        maven("https://maven.aliyun.com/repository/public/")
+    }
+    // 构建脚本需要用到的三方库
+    dependencies {
+        "classpath"(group = "commons-codec", name = "commons-codec", version = "1.16.0")
+    }
+}
+
 plugins {
     id("java")
 }
 extra["hasTests"] = true
+
+tasks.register("encode") {
+    doLast {
+        // 使用buildscript中引入的依赖
+        val encodedString = Base64().encode("hello world\n".toByteArray())
+        println(String(encodedString))
+    }
+}
 
