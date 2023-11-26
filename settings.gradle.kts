85028a6af68e6e976ce1e246be2fd09304a1e0a3
justin <pulchritude007@163.com>
2023-11-26 21:11:19 +0800

    update setting.gradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,12 +15,10 @@ dependencyResolutionManagement {
      * 不用配置allprojects或subprojects或子项目中指定，此处统一配置仓库
      */
     repositories {
-        repositories {
-            maven("https://maven.aliyun.com/repository/public/")
-            maven("https://mirrors.huaweicloud.com/repository/maven/")
-            // 或者公司本地仓库
-            mavenCentral()
-        }
+        maven("https://maven.aliyun.com/repository/public/")
+        maven("https://mirrors.huaweicloud.com/repository/maven/")
+        // 或者公司本地仓库
+        mavenCentral()
     }
     // 默认值
     repositoriesMode = RepositoriesMode.PREFER_PROJECT
