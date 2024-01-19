615fc454da4076e3eb0d499a4e3f406c9caaedf1
justin <pulchritude007@163.com>
2024-01-19 18:28:00 +0800

    comment

--- dubbo-samples/dubbo-consumer/src/main/java/com/example/consumer/stub/StubController.java
+++ dubbo-samples/dubbo-consumer/src/main/java/com/example/consumer/stub/StubController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class StubController {
     private static Logger logger = LoggerFactory.getLogger(StubController.class);
 
-    // stub指定stub实现
+    // stub指定stub实现， stub实现必须有构造函数传入真正的远程代理对象
     @DubboReference(group = "dg", version = "1.0.0", check=false, stub="com.example.client.stub.StubServiceStub")
     private StubService demoService;
 
