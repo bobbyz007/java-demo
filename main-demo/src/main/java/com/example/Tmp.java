ae390cf3efd2bdff5878afc136c007e5fb6a7258
justin <pulchritude007@163.com>
2024-02-23 17:27:12 +0800

    fix

--- main-demo/src/main/java/com/example/Tmp.java
+++ main-demo/src/main/java/com/example/Tmp.java
@@ -1,12 +1,15 @@
 package com.example;
 
+import java.net.UnknownHostException;
+
 /**
  * 临时测试类：随时可能删除
  */
 public class Tmp {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, UnknownHostException {
         //Thread.currentThread().join();
         System.out.println(Tmp.class.getName());
+        System.out.println(java.net.InetAddress.getLocalHost().getCanonicalHostName());
     }
 }
 
